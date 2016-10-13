35caada13f9887828e0fb6e4fd06fc123e6df3ed
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2016-10-13 20:53:12 +0200

    Update actualizar.sh

--- actualizar.sh
+++ actualizar.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # actualizador de paguetes para Debian
 # actualizar.sh Programado en shell
-# boton derecho para dar permisos del ejecucion 
+# boton derecho para dar permisos del ejecucion o desde consola teclemaos: chmod 777 actualizar.sh
 # se ejecuta en consola ./actualizar.sh
 # Antonio Taboada
 # 3 Sept 2016
