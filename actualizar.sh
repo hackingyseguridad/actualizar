5f09cab18a504c1907a29b6897bc18a2137d324e
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2017-04-03 23:34:32 +0200

    Add files via upload

--- actualizar.sh
+++ actualizar.sh
@@ -3,11 +3,6 @@
 # actualizar.sh Programado en shell
 # boton derecho para dar permisos del ejecucion o desde consola teclemaos: chmod 777 actualizar.sh
 # se ejecuta en consola ./actualizar.sh
-# Para ejecutar en el arranque de linux copiar a initd.d y actualizar el incio
-# cp actualizar.sh /etc/init.d
-# cd /etc/init.d
-# chmod 777 actualizar.sh
-# update-rc.d actualizar.sh defaults
 # Antonio Taboada
 # 3 Sept 2016
 
---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Actualizador de paguetes para Debian
+# Se instalara para ejecutarse y actualizar de forma automatica al inicio
+cp actualizar.sh /etc/init.d/
+cd /etc/init.d
+chmod 777 actualizar.sh
+update-rc.d actualizar.sh defaults
