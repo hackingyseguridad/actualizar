9cd0b92fd14365df56871f78ebad7161d48d956c
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2017-06-05 15:47:43 +0200

    Update and rename install.sh to instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Actualizador de paguetes para Debian
+# Actualizador de paguetes para Debian 
 # Se instalara para poder ser ejecutardo como un comando de Linux
 chmod 777 actualizar
 cp actualizar /sbin/
