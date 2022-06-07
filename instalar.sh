8cd5cd2e9821c06688921ee9395019d9126e578a
hackingyseguridad <antonio_taboada@hotmail.es>
2022-06-07 15:13:09 +0200

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 # Actualizador de paguetes para distrubuciones basadas en Debian 
 # Se instalara en la carperta /sbin/ para poder ser invocado y ejecutardo como un comando de Linux
+echo
+echo "(c) hackingyseguridad.com 2022"
+echo
+echo "Instalando ..."
+echo
+echo "Estableciendo hora"
+timedatectl set-timezone Europe/Madrid
+timedatectl timedatectl set-local-rtc 0
+timedatectl
+echo
+echo "Copiando ficheros ..."
 chmod 777 actualizar
 cp actualizar /sbin/
 chmod 777 actualiza
 cp actualiza /sbin/
+echo
+echo "Instalado !!!"
