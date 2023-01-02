d57788320664321c522b105ea8705fa1fee8f29b
hackingyseguridad <antonio_taboada@hotmail.es>
2023-01-02 16:01:41 +0100

    Update actualizar.sh

--- actualizar.sh
+++ actualizar.sh
@@ -1,4 +1,6 @@
-
+#!/bin/sh
+# Actualizador online de paguetes para distribuciones basadas en Debian
+# Antonio Taboada 2023 - hackingyseguridad.com
 cd /tmp/
 chmod 777 actualizar
 wget https://raw.githubusercontent.com/hackingyseguridad/actualizar/master/actualizar -q -O - | bash
