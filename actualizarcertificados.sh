f70fb3d0b5f53f80899efde81284ba873fc73918
hackingyseguridad <antonio_taboada@hotmail.es>
2023-01-18 19:08:06 +0100

    Create actualizarcertificados.sh

---
+++ actualizarcertificados.sh
@@ -0,0 +1,9 @@
+echo 
+echo descargar certificados actuales a intalarlos
+echo "ultima version de certificados para Debian "
+echo "http://ftp.debian.org/debian/pool/main/c/ca-certificates/?C=M;O=D"
+echo 
+
+wget https://security.debian.org/debian-security/pool/main/c/ca-certificates/ca-certificates_20200601~deb9u2_all.deb
+
+sudo dpkg -i ca-certificates_20200601~deb9u2_all.deb
