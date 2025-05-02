a2cdc804e88e3c7f4faca316fd534cd9c51d5a16
hackingyseguridad <antonio_taboada@hotmail.es>
2025-05-02 11:18:26 +0200

    Update actualizarcertificados.sh

--- actualizarcertificados.sh
+++ actualizarcertificados.sh
@@ -5,6 +5,13 @@ echo "ultima version de certificados para Debian "
 echo "http://ftp.debian.org/debian/pool/main/c/ca-certificates/?C=M;O=D"
 echo 
 
-wget https://security.debian.org/debian-security/pool/main/c/ca-certificates/ca-certificates_20200601~deb9u2_all.deb
+# instala nuevas claves PGP Abril 2025
 
+cd /tmp/
+sudo wget https://archive.kali.org/archive-keyring.gpg -O /usr/share/keyrings/kali-archive-keyring.gpg
+gpg --no-default-keyring --keyring /usr/share/keyrings/kali-archive-keyring.gpg -k
+
+# instala certificados !!
+
+wget https://security.debian.org/debian-security/pool/main/c/ca-certificates/ca-certificates_20200601~deb9u2_all.deb
 sudo dpkg -i ca-certificates_20200601~deb9u2_all.deb
